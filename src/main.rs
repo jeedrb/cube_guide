62a2b1bfc7cd47eac36ea0b65eb749f66f9e5979
jeedrb <jar3d.xd@gmail.com>
2024-02-29 13:45:01 -0500

    starting ordering

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use mysql::*;
 use mysql::prelude::*;
 use std::io;
 use std::cmp::Ordering;
+use std::collections::HashMap;
 
 struct Cube {
     make: String, // Gan
@@ -24,18 +25,44 @@ fn main() {
             Cube {make, model, price, magnets, magnetic_core, description}
         },
     ).unwrap();
-    
+
+    // creating string vectors to put later dump into hashmaps for valid options
+    let mut valid_dir_asc_vec: Vec<&str> = vec!["a", "asc", "ascend", "ascending"];
+    let mut valid_dir_desc_vec: Vec<&str> = vec!["d", "desc", "descend", "descending"];
+    let mut valid_sorts_vec: Vec<&str> = vec!["make", "model", "price", "magnets", "magnetic_core"];
+
+    // creating hashmaps and dumping the vector data into them
+    let mut valid_dir_asc = HashMap::new();
+    for &i in &valid_dir_asc_vec {
+        valid_dir_asc.insert(i, i);
+    }
+
+    let mut valid_dir_desc = HashMap::new();
+    for &i in &valid_dir_desc_vec {
+        valid_dir_desc.insert(i, i);
+    }
+
+    let mut valid_sorts = HashMap::new();
+    for &i in &valid_sorts_vec {
+        valid_sorts.insert(i, i);
+    }
+
     // getting input on how to sort the results
     let mut sort_selection = String::new();
     io::stdin()
         .read_line(&mut sort_selection)
-        .expect("try again dummy");
+        .expect("input error");
     
+    let mut sort_direction = String::new();
+    io::stdin()
+        .read_line(&mut sort_direction)
+        .expect("input error");
+
     // first trims edge whitespace, then makes it all lowercase, then converts the remaining spaces to underscores
     let sort_selection = sort_selection.trim().to_lowercase().replace(" ", "_");
+    let sort_direction = sort_direction.trim().to_lowercase().replace(" ", "_");
 
     // sorting based on input
-    // TODO be able to choose between asc/desc sort
     cubes.sort_by(|a, b| match sort_selection.as_str() {
         "make" => a.make.cmp(&b.make),
         "model" => a.model.cmp(&b.model),
@@ -46,8 +73,10 @@ fn main() {
         _ => panic!("Invalid field: {}", sort_selection),
     });
 
+    
+
     let mut counter: i32 = 0;
-    for x in cubes {
+    for x in cubes.iter().rev() { // actually implement selecting an order lol
         counter += 1;
         println!("{counter} {} {}", x.make, x.model);
     }
